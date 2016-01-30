e4f1a9c895302c7b181fc75f5f60078d0fe4d97c
knolegsb <seon2you@hotmail.com>
2016-01-29 16:44:19 -0800

    start

---
+++ EFCodeFirstPractice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFCodeFirstPractice.MvcApplication" Language="C#" %>
---
+++ EFCodeFirstPractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EFCodeFirstPractice.Startup))]
+namespace EFCodeFirstPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
